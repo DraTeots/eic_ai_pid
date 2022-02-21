561898db93a0ba97a72e5fe48515ce6f9f7d8f44
Espen <espenlunden81@gmail.com>
2022-02-21 18:03:57 -0500

    Auto generated layers

--- keras_geant3_autoencoder.py
+++ keras_geant3_autoencoder.py
@@ -21,7 +21,7 @@ parse_start = time.time()
 print(f"Start preparing events...")
 
 add_real_xy = False
-inputs, true_e, sum_e = build_train_set(data_file, 200000, add_real_xy=add_real_xy, normalize=False)
+inputs, true_e, sum_e = build_train_set(data_file, 10000, add_real_xy=add_real_xy, normalize=False)
 parse_end = time.time()
 
 print(f"Total events prepare time = {parse_end - parse_start}")
@@ -35,27 +35,17 @@ from keras.layers import Dense
 # # define the keras model
 model = Sequential()
 
-input_dim = 123
-middle_layer_size = 20
-hidden_layers_num = 5
+# model.add(Dense(123, input_dim=123, activation='gelu'))
+# model.add(Dense(90, activation='gelu'))
+# model.add(Dense(80, activation='gelu'))
+# model.add(Dense(70, activation='gelu'))
+# model.add(Dense(50, activation='gelu'))
+# model.add(Dense(70, activation='gelu'))
+# model.add(Dense(80, activation='gelu'))
+# model.add(Dense(90, activation='gelu'))
+# model.add(Dense(123, activation='linear'))
 
-# go from 20 to 123 in 5 steps (or from 123 to 20)
-
-
-model.add(Dense(123, input_dim=123, activation='gelu'))
-model.add(Dense(90, activation='gelu'))
-model.add(Dense(80, activation='gelu'))
-model.add(Dense(70, activation='gelu'))
-model.add(Dense(50, activation='gelu'))
-model.add(Dense(70, activation='gelu'))
-model.add(Dense(80, activation='gelu'))
-model.add(Dense(90, activation='gelu'))
-model.add(Dense(123, activation='linear'))
-
-
-
-
-# More extensie model
+# More extensive model
 # model.add(Dense(123, input_dim=123, activation='selu'))
 # model.add(Dense(90, activation='selu'))
 # model.add(Dense(60, activation='selu'))
@@ -64,12 +54,33 @@ model.add(Dense(123, activation='linear'))
 # model.add(Dense(10, activation='selu'))
 # model.add(Dense(1, activation='linear'))
 
+input_dims = 123
+middle_layer_size = 40
+hidden_layers_num = 3
+
+# go from 20 to 123 in 5 steps (or from 123 to 20)
+
+delta_dim = int((input_dims - middle_layer_size) / hidden_layers_num)
+
+model.add(Dense(input_dims, input_dim=123, activation='gelu'))
+
+for i in range(1, hidden_layers_num):
+    neurons_num =  input_dims - i*delta_dim
+    model.add(Dense(neurons_num, activation='gelu'))
+
+for i in range(hidden_layers_num):
+    neurons_num =  middle_layer_size + i*delta_dim
+    model.add(Dense(neurons_num, activation='gelu'))
+
+model.add(Dense(input_dims, activation='linear'))
+
+
 # compile the keras model
 # model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['acc', 'mse', 'mae'])
 model.compile(loss='binary_crossentropy', optimizer='nadam', metrics=['acc', 'mse', 'mae'])
 
 # fit the keras model on the dataset
-history = model.fit(inputs, inputs, validation_split=0.05, epochs=40, batch_size=32, verbose=1)
+history = model.fit(inputs, inputs, validation_split=0.05, epochs=20, batch_size=32, verbose=1)
 
 # Save everything
 name = "g3_autoencoder_with_xy" if add_real_xy else "g3_autoencoder_no_xy"
