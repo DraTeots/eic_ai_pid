50d3e9cc71e50ebbf5b55a8aa0fbc4223aab38ce
Dmitry Romanov <romanovda@gmail.com>
2022-04-04 18:52:05 -0400

    True-answer table network works!

--- geant3_parser.py
+++ geant3_parser.py
@@ -5,11 +5,9 @@ from itertools import zip_longest  # for Python 3.x
 import os
 import numpy as np
 import math
+import random
 
-max_cell_index = 5
-total_columns = max_cell_index * 2 + 1
-total_rows = total_columns              # square calorimeter
-event_size = total_columns * total_rows + 2  # 2 for real_x, real_y
+
 
 class Geant3DataFile:
     max_cell_index = 32
@@ -77,13 +75,20 @@ def process_events(data_file):
 
 def build_train_set(data_file, events_to_process=40000, add_real_xy=True, normalize=True):
     assert isinstance(data_file, Geant3DataFile)
+
+    # Some constants    
+    max_cell_index = 5
+    total_columns = max_cell_index * 2 + 1
+    total_rows = total_columns              # square calorimeter
+    event_size = total_columns * total_rows + 2  # 2 for real_x, real_y
+
+    
     input_values = np.zeros((events_to_process, event_size))
     true_values = np.zeros((events_to_process, 1))
     sum_values = np.zeros((events_to_process, 1))
 
     data = itertools.islice(data_file.next_event(), events_to_process)
     
-
     for event_index, event in enumerate(data):
         real_x, real_y, real_e, hits = event
 
@@ -103,8 +108,64 @@ def build_train_set(data_file, events_to_process=40000, add_real_xy=True, normal
         sum_values[event_index][0] = sum_e
         true_values[event_index][0] = real_e
 
-    return input_values, true_values, sum_values        
+    return input_values, true_values, sum_values
 
+
+def build_true_answers_train_set(data_file, events_to_process=40000, norm_func=None, rows=11, cols=11, rnd_shift=None):
+    """
+    rnd_shift = ((row_min, row_max), (col_min, col_max)) where row,col=(0,0) is a center
+    """
+    assert isinstance(data_file, Geant3DataFile)
+    input_values = np.zeros((events_to_process, rows, cols))
+    true_values = np.zeros((events_to_process, rows, cols))
+    e_values = np.zeros((events_to_process, 2))
+    central_row = int(rows/2)
+    central_col = int(cols/2)
+
+    data = itertools.islice(data_file.next_event(), events_to_process)
+    
+
+    for event_index, event in enumerate(data):
+        real_x, real_y, real_e, hits = event
+
+        #input_values[event_index, 0] = real_x if add_real_xy else 0
+        #input_values[event_index, 1] = real_y if add_real_xy else 0
+        sum_e = 0
+
+        # Position
+        row_shift = 0
+        col_shift = 0
+        if rnd_shift:
+            row_shift = random.randint(rnd_shift[0][0], rnd_shift[0][1])
+            col_shift = random.randint(rnd_shift[1][0], rnd_shift[1][1])
+
+        # Going for each hit
+        for col, row, e in hits:
+            # Energy
+            if e == 0:
+                continue
+            
+            norm_e = norm_func(e) if norm_func else e
+            sum_e += e
+
+            row_index = central_row + row_shift + row
+            col_index = central_col + col_shift + col
+
+            # >oO
+            # print(f"row_index({row_index}) = central_row({central_row}) + row_shift({row_shift}) + row({row})")
+            # print(f"col_index({col_index}) = central_col({central_col}) + col_shift({col_shift}) + col({col})")
+            # print(f"norm_e={norm_e} e={e}")
+
+            if row_index >= 0 and row_index < rows and col_index >= 0 and col_index < cols:
+                input_values[event_index, row_index, col_index] = norm_e
+            
+        
+        true_values[event_index, central_row + row_shift, central_col + col_shift] = norm_func(sum_e) if norm_func else real_e
+        
+        e_values[event_index][0] = real_e
+        e_values[event_index][1] = sum_e
+
+    return input_values, true_values, e_values
 
 def test_data_file():
     file_name = os.path.join('data', 'shower_geant3_new.dat')
